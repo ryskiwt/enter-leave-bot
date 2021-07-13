da30e92c5cc546803448f83c7debfb371a28fe66
ryskiwt <iw.06.ry.10@gmail.com>
2021-07-14 00:17:01 +0900

    misc: linterエラーを修正

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,18 @@
+import json
 import os
-import requests, json
 
+import requests
 import discord
 
 
-token = os.environ["DISCORD_BOT_TOKEN"] # https://discord.com/developers/applications から取得
-channel_id = os.environ["DISCORD_TARGET_CHANNEL_ID"] # Discordを開発者モードにしてチャンネルを右クリックで取得できる
-webhook_url = os.environ["SLACK_WEBHOOK_URL"] # Slack に Incoming Webhook を追加して取得
+# https://discord.com/developers/applications から取得
+token = os.environ["DISCORD_BOT_TOKEN"]
+
+# Discordを開発者モードにしてチャンネルを右クリックで取得できる
+channel_id = os.environ["DISCORD_TARGET_CHANNEL_ID"]
+
+# Slack に Incoming Webhook を追加して取得
+webhook_url = os.environ["SLACK_WEBHOOK_URL"]
 
 
 client = discord.Client()
@@ -23,16 +29,21 @@ async def on_voice_state_update(member, before, after):
 
         # 入室
         if after.channel is not None and after.channel.id == channel_id:
-            msg = f"{before.channel.name} に、 {member.name} が入室しました。\n\n" \
-                "現在の参加者:\n  " \
+            msg = (
+                f"{before.channel.name} に、 {member.name} が入室しました。\n\n"
+                "現在の参加者:\n  "
                 "\n  ".join([m.name for m in after.channel.members])
+            )
             post_to_slack(msg)
 
         # 退室
         if before.channel is not None and before.channel.id == channel_id:
-            msg = f"{before.channel.name} から、 {member.name} が退室しました。\n\n" \
-                "現在の参加者:\n  " \
+            msg = (
+                f"{before.channel.name} から、 {member.name} が退室しました。\n\n"
+                "現在の参加者:\n  "
                 "\n  ".join([m.name for m in before.channel.members])
+            )
             post_to_slack(msg)
+
 
 client.run(token)
