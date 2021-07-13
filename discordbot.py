4dfe95513dabde0528eeb49b99a81a99eeac1f10
ryskiwt <iw.06.ry.10@gmail.com>
2021-07-13 23:59:34 +0900

    mod: 入退出監視処理を実装

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,38 @@
-from discord.ext import commands
 import os
-import traceback
+import requests, json
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+import discord
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+token = os.environ["DISCORD_BOT_TOKEN"] # https://discord.com/developers/applications から取得
+channel_id = os.environ["DISCORD_TARGET_CHANNEL_ID"] # Discordを開発者モードにしてチャンネルを右クリックで取得できる
+webhook_url = os.environ["SLACK_WEBHOOK_URL"] # Slack に Incoming Webhook を追加して取得
 
 
-bot.run(token)
+client = discord.Client()
+
+
+def post_to_slack(msg):
+    requests.post(webhook_url, data=json.dumps({"text": msg}))
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+
+    if before.channel != after.channel:
+
+        # 入室
+        if after.channel is not None and after.channel.id == channel_id:
+            msg = f"{before.channel.name} に、 {member.name} が入室しました。\n\n" \
+                "現在の参加者:\n  " \
+                "\n  ".join([m.name for m in after.channel.members])
+            post_to_slack(msg)
+
+        # 退室
+        if before.channel is not None and before.channel.id == channel_id:
+            msg = f"{before.channel.name} から、 {member.name} が退室しました。\n\n" \
+                "現在の参加者:\n  " \
+                "\n  ".join([m.name for m in before.channel.members])
+            post_to_slack(msg)
+
+client.run(token)
